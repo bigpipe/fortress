273576b913f1e48e7adc431a601ca03e7acdb9d8
3rd-Eden <info@3rd-Eden.com>
2013-11-29 00:17:42 +0100

    [test] Use `bdd`

--- test.js
+++ test.js
@@ -4,14 +4,24 @@
 var Fortress = require('./index.js')
   , assert = require('assert');
 
-suite('fortress');
+describe('fortress', function () {
+  it('exposes the Container', function () {
+    assert.equal(typeof Fortress.Container, 'function');
+  });
 
-test('it should not throw an error when a new instance is created', function () {
-  var fort = new Fortress();
-});
+  it('exposes the Image', function () {
+    assert.equal(typeof Fortress.Image, 'function');
+  });
 
-test('it should create a new fortress instance if its not created using new', function () {
-  var fort = Fortress();
+  it('should not throw an error when a new instance is created', function () {
+    var fort = new Fortress();
 
-  assert.ok(fort instanceof Fortress, 'should be an instance of Fortress');
+    assert.ok(fort instanceof Fortress);
+  });
+
+  it('should create a new fortress instance if its not created using new', function () {
+    var fort = Fortress();
+
+    assert.ok(fort instanceof Fortress, 'should be an instance of Fortress');
+  });
 });
